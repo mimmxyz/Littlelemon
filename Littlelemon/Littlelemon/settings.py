d758479e907a8bd728c27f551d340eaf600574bf
mimmxyz <137398086+mimmxyz@users.noreply.github.com>
2023-11-28 13:17:32 +0100

    Update settings.py
    switch to default user root , pass root in mysql

--- Littlelemon/Littlelemon/settings.py
+++ Littlelemon/Littlelemon/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
         'NAME': 'reservations',
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
-        'USER' : 'django',
-        'PASSWORD' : 'Pensiero1?',
+        'USER' : 'root',
+        'PASSWORD' : 'root',
     }
 }
 
